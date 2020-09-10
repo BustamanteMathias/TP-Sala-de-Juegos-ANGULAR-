c9dcc168bb2de3a8b0e2953228a88199e68c0b3b
Bustamante Mathias <bustamantemathias.tec@gmail.com>
2020-09-10 19:59:55 -0300

    Heroku config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const path = requiere('path');
+const path = require('path');
 
-
-app.use(express.static(__dirname + '/src/app/'))
+app.use(express.static(__dirname + '/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/app/app.component.html'));
+app.get('/*', function (req, res) {
+   res.sendfile(path.join(__dirname + '/angularapp/index.html'));
 });
+
+console.log('Build successful!!');
