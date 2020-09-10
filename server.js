9c59528e088b65f231750c87b89a436c9a5653a3
Bustamante Mathias <bustamantemathias.tec@gmail.com>
2020-09-10 19:18:01 -0300

    Heroku config

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = requiere('path');
+
+
+app.use(express.static(__dirname + '/src/app/'))
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/app/app.component.html'));
+});
